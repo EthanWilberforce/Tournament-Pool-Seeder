d671fbe6c3d746fd18e2342d1cc249ac5a1a19f0
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2015-01-30 12:47:34 -0500

    Added base web layout, js beginnings

---
+++ main.js
@@ -0,0 +1,14 @@
+var RAWentrants = "";
+var entrants = [];
+var entPerPool = 5;
+var numPools = entrants.length / entPerPool;
+var count = true;
+
+//need to split raw entrants by every , and strip white space, = entrants[x]
+
+
+for (count ; count <= entrants.length ; count++){
+  //need to place entrant names in the correct latex locations
+}
+
+//pass pdf to user
