4e17aa1c0915910c4591caa7ebb225d825941af7
Ethan Wilberforce <ethan.wilberforce@gmail.com>
2015-01-30 14:10:42 -0500

    added split and trim function

--- main.js
+++ main.js
@@ -14,11 +14,8 @@ var entPerPool = 5;
 var numPools = entrants.length / entPerPool;
 var count = true;
 
-//need to split raw entrants by every , and strip white space, = entrants[x]
-
-
 for (count ; count <= entrants.length ; count++){
-  //need to place entrant names in the correct latex locations
+  //need to place trimmed entrant names in the correct latex locations
 }
 
 //pass pdf to user
